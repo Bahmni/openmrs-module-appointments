1014063a30b630f9d942ee04ee8c60588403cf52
pramidatumma <pramidat@thoughtworks.com>
2017-08-11 18:59:02 +0530

    Santhosh, Pramida|#000|Fixing dateUtil tests for CI pipeline

--- api/src/test/java/org/openmrs/module/appointments/util/DateUtilTest.java
+++ api/src/test/java/org/openmrs/module/appointments/util/DateUtilTest.java
@@ -7,8 +7,11 @@ import org.junit.Test;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class DateUtilTest {
@@ -28,6 +31,10 @@ public class DateUtilTest {
     @Test
     public void shouldConvertStringtoDate() throws ParseException {
         Date date = DateUtil.convertToDate("2017-03-15T16:57:09.0Z", DateUtil.DateFormatType.UTC);
-        assertEquals("Wed Mar 15 16:57:09 IST 2017", date.toString());
+        assertNotNull(date);
+        boolean daylightTime = TimeZone.getDefault().inDaylightTime(date);
+        String timeZoneShort = TimeZone.getDefault().getDisplayName(daylightTime, TimeZone.SHORT, Locale.ENGLISH);
+        String expectedDateString = "Wed Mar 15 16:57:09 ".concat(timeZoneShort).concat(" 2017");
+        assertEquals(expectedDateString, date.toString());
     }
 }
