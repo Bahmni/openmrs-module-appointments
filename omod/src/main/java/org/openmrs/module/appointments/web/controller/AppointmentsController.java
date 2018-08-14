82a784065fc383c09531e657c82116b20ab8b8f9
sumanmaity112 <suman.maity112@gmail.com>
2018-08-14 20:09:29 +0530

    Suman | BAH-563 | Move {UUID}/changeStatus api to new AppointmentsController

--- omod/src/main/java/org/openmrs/module/appointments/web/controller/AppointmentsController.java
+++ omod/src/main/java/org/openmrs/module/appointments/web/controller/AppointmentsController.java
@@ -2,12 +2,20 @@ package org.openmrs.module.appointments.web.controller;
 
 import org.openmrs.module.appointments.model.Appointment;
 import org.openmrs.module.appointments.service.AppointmentsService;
+import org.openmrs.module.appointments.util.DateUtil;
 import org.openmrs.module.appointments.web.contract.AppointmentDefaultResponse;
 import org.openmrs.module.appointments.web.mapper.AppointmentMapper;
 import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.RestUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/appointments")
@@ -17,14 +25,32 @@ public class AppointmentsController {
     @Autowired
     private AppointmentMapper appointmentMapper;
 
-    @RequestMapping(method = RequestMethod.GET, value="/{uuid}")
+    @RequestMapping(method = RequestMethod.GET, value = "/{uuid}")
     @ResponseBody
-    public AppointmentDefaultResponse getAppointmentByUuid(@PathVariable(value = "uuid") String uuid)  {
+    public AppointmentDefaultResponse getAppointmentByUuid(@PathVariable(value = "uuid") String uuid) {
         Appointment appointment = appointmentsService.getAppointmentByUuid(uuid);
-        if(appointment == null){
+        if (appointment == null) {
             throw new RuntimeException("Appointment does not exist");
         }
         return appointmentMapper.constructResponse(appointment);
     }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/{uuid}/changeStatus")
+    @ResponseBody
+    public ResponseEntity<Object> transitionAppointment(@PathVariable("uuid") String appointmentUuid, @RequestBody Map<String, String> statusDetails) throws ParseException {
+        try {
+            String toStatus = statusDetails.get("toStatus");
+            Date onDate = DateUtil.convertToLocalDateFromUTC(statusDetails.get("onDate"));
+            Appointment appointment = appointmentsService.getAppointmentByUuid(appointmentUuid);
+            if (appointment != null) {
+                appointmentsService.changeStatus(appointment, toStatus, onDate);
+                return new ResponseEntity<>(appointmentMapper.constructResponse(appointment), HttpStatus.OK);
+            } else
+                throw new RuntimeException("Appointment does not exist");
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(RestUtil.wrapErrorResponse(e, e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+
 
 }
--- omod/src/test/java/org/openmrs/module/appointments/web/controller/AppointmentsControllerIT.java
+++ omod/src/test/java/org/openmrs/module/appointments/web/controller/AppointmentsControllerIT.java
@@ -4,12 +4,30 @@ import org.codehaus.jackson.type.TypeReference;
 import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.appointments.dao.AppointmentAuditDao;
+import org.openmrs.module.appointments.model.Appointment;
+import org.openmrs.module.appointments.model.AppointmentAudit;
+import org.openmrs.module.appointments.service.AppointmentsService;
+import org.openmrs.module.appointments.util.DateUtil;
 import org.openmrs.module.appointments.web.BaseIntegrationTest;
 import org.openmrs.module.appointments.web.contract.AppointmentDefaultResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class AppointmentsControllerIT extends BaseIntegrationTest {
+
+    @Autowired
+    AppointmentsService appointmentsService;
+
+    @Autowired
+    AppointmentAuditDao appointmentAuditDao;
 
     @Before
     public void setUp() throws Exception {
@@ -25,4 +43,58 @@ public class AppointmentsControllerIT extends BaseIntegrationTest {
                 });
         assertEquals("GAN200000", response.getPatient().get("identifier"));
     }
+
+    @Test
+    public void should_changeAppointmentStatusWithDate() throws Exception {
+        String onDate = "2108-08-22T10:30:00.0Z";
+        String content = "{ \"toStatus\": \"CheckedIn\", \"onDate\":\""+ onDate +"\"}";
+        MockHttpServletResponse response = handle(newPostRequest("/rest/v1/appointments/c36006e5-9fbb-4f20-866b-0ece245615a7/changeStatus", content));
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        Appointment appointmentByUuid = appointmentsService.getAppointmentByUuid("c36006e5-9fbb-4f20-866b-0ece245615a7");
+        assertNotNull(appointmentByUuid);
+
+        List<AppointmentAudit> historyForAppointment = appointmentAuditDao.getAppointmentHistoryForAppointment(appointmentByUuid);
+        assertEquals(1, historyForAppointment.size());
+        assertNotNull(historyForAppointment.get(0).getId());
+        assertNotNull(historyForAppointment.get(0).getNotes());
+        assertEquals(DateUtil.convertToLocalDateFromUTC(onDate).toInstant().toString(), historyForAppointment.get(0).getNotes());
+    }
+
+    @Test
+    public void should_changeAppointmentStatusWithoutDate() throws Exception {
+        String content = "{ \"toStatus\": \"CheckedIn\"}";
+        MockHttpServletResponse response = handle(newPostRequest("/rest/v1/appointments/c36006e5-9fbb-4f20-866b-0ece245615a7/changeStatus", content));
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        Appointment appointmentByUuid = appointmentsService.getAppointmentByUuid("c36006e5-9fbb-4f20-866b-0ece245615a7");
+        assertNotNull(appointmentByUuid);
+        List<AppointmentAudit> historyForAppointment = appointmentAuditDao
+                .getAppointmentHistoryForAppointment(appointmentByUuid);
+        assertEquals(1, historyForAppointment.size());
+        assertNotNull(historyForAppointment.get(0).getDateCreated());
+        assertNotNull(historyForAppointment.get(0).getCreator());
+        assertEquals(appointmentByUuid, historyForAppointment.get(0).getAppointment());
+        assertEquals(appointmentByUuid.getStatus(), historyForAppointment.get(0).getStatus());
+        assertNull(historyForAppointment.get(0).getNotes());
+    }
+
+    @Test
+    public void should_throwExceptionForInvalidStatusChange() throws Exception {
+        String content = "{ \"toStatus\": \"Scheduled\"}";
+        MockHttpServletResponse response = handle(newPostRequest("/rest/v1/appointments/c36006e5-9fbb-4f20-866b-0ece245615a7/changeStatus", content));
+        assertNotNull(response);
+        assertEquals(400, response.getStatus());
+    }
+
+    @Test
+    public void should_throwExceptionForInvalidAppointment() throws Exception {
+        String content = "{ \"toStatus\": \"Scheduled\"}";
+        MockHttpServletResponse response = handle(newPostRequest("/rest/v1/appointments/c36006e5-9fbb-4f20-866b-0ece245615a8/changeStatus", content));
+        assertNotNull(response);
+        assertEquals(400, response.getStatus());
+    }
+
+
 }
+
