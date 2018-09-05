2d9caa31a025ab55e4ff02188c7444c81653e866
Saikumar <saikv@thoughtworks.com>
2018-09-05 15:03:21 +0530

    BAH-625 | Remove self appointment role and rename privilege as Manage Own Appointments

--- api/src/main/resources/addPrivilegesToManageSelfAppointmentsRole.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageSelfAppointments','app:appointments');
-INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageSelfAppointments','Get Locations');
-INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageSelfAppointments','Get Patient Identifiers');
-INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageSelfAppointments','Get Patients');
-INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageSelfAppointments','Get Providers');
-INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageSelfAppointments','View Appointment Services');
-INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageSelfAppointments','View Appointments');
-INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageSelfAppointments','Manage Self Appointments');
-INSERT INTO role_privilege (role, privilege) VALUES ('Appointments:ManageSelfAppointments','app:appointments:manageSelfAppointmentsOnly');
