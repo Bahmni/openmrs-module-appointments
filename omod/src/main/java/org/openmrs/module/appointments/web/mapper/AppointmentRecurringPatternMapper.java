aaaf0b4aa549b83a58881bf5656843f95bcc92c1
Alekhya <alekhyayalla23@gmail.com>
2019-07-18 14:36:46 +0530

    Sneha, Alekhya | MOBN-576 | Refactor and create AppointmentRecurringPatternMapper, abstract out validate & save in appointment service

---
+++ omod/src/main/java/org/openmrs/module/appointments/web/mapper/AppointmentRecurringPatternMapper.java
@@ -0,0 +1,34 @@
+package org.openmrs.module.appointments.web.mapper;
+
+import org.openmrs.module.appointments.model.AppointmentRecurringPattern;
+import org.openmrs.module.appointments.web.contract.AppointmentRequest;
+import org.openmrs.module.appointments.web.contract.RecurringPattern;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+import static org.openmrs.module.appointments.service.impl.RecurringAppointmentType.DAY;
+import static org.openmrs.module.appointments.service.impl.RecurringAppointmentType.WEEK;
+import static org.openmrs.module.appointments.service.impl.RecurringAppointmentType.valueOf;
+
+@Component
+public class AppointmentRecurringPatternMapper {
+
+    public AppointmentRecurringPattern fromRequestRecurringPattern(RecurringPattern recurringPattern) {
+        AppointmentRecurringPattern appointmentRecurringPattern = new AppointmentRecurringPattern();
+        appointmentRecurringPattern.setEndDate(recurringPattern.getEndDate());
+        appointmentRecurringPattern.setPeriod(recurringPattern.getPeriod());
+        appointmentRecurringPattern.setFrequency(recurringPattern.getFrequency());
+        String recurringPatternType = recurringPattern.getType();
+        if (recurringPatternType == null) {
+            throw new IllegalArgumentException(String
+                    .format("Valid recurrence type should be provided. Valid types are %s and %s",  DAY, WEEK));
+        }
+        appointmentRecurringPattern.setType(valueOf(recurringPatternType.toUpperCase()));
+        if (appointmentRecurringPattern.getType() == WEEK) {
+            appointmentRecurringPattern.setDaysOfWeek(recurringPattern.getDaysOfWeek().stream().map(String::toUpperCase)
+                    .collect(Collectors.joining(",")));
+        }
+        return appointmentRecurringPattern;
+    }
+}
