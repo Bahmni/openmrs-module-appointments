aafe5b562eda36ab5b5d6c0f6cd64ab8e7a12f61
Sneha Bagri <snehabagri90@gmail.com>
2019-07-22 16:07:00 +0530

    Vineela, Sneha Bagri | MOBN-576 | Write method to get AppointmentRecurringPattern from appointment request for Single Recurring Appointment

--- omod/src/main/java/org/openmrs/module/appointments/web/mapper/SingleAppointmentRecurringPatternMapper.java
+++ omod/src/main/java/org/openmrs/module/appointments/web/mapper/SingleAppointmentRecurringPatternMapper.java
@@ -1,16 +1,59 @@
 package org.openmrs.module.appointments.web.mapper;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.openmrs.api.APIException;
+import org.openmrs.module.appointments.model.Appointment;
 import org.openmrs.module.appointments.model.AppointmentRecurringPattern;
+import org.openmrs.module.appointments.service.AppointmentsService;
 import org.openmrs.module.appointments.web.contract.AppointmentRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashSet;
 
 @Component
 @Qualifier("singleAppointmentRecurringPatternMapper")
 public class SingleAppointmentRecurringPatternMapper extends AbstractAppointmentRecurringPatternMapper {
 
+    @Autowired
+    private AppointmentsService appointmentsService;
+
+    @Autowired
+    private AppointmentMapper appointmentMapper;
+
     @Override
     public AppointmentRecurringPattern fromRequest(AppointmentRequest appointmentRequest){
-        return null;
+        String uuid = appointmentRequest.getUuid();
+        Appointment appointment = appointmentsService.getAppointmentByUuid(uuid);
+        if(appointment==null)
+            throw new APIException("Appointment does not exist.");
+
+        Appointment newAppointment = getNewAppointmentFrom(appointment);
+        appointment.setVoided(true);
+        newAppointment.setRelatedAppointment(appointment);
+        appointmentMapper.mapAppointmentRequestToAppointment(appointmentRequest, newAppointment);
+        final AppointmentRecurringPattern appointmentRecurringPattern = appointment.getAppointmentRecurringPattern();
+        appointmentRecurringPattern.getAppointments().add(newAppointment);
+        return appointmentRecurringPattern;
     }
+
+
+    private Appointment getNewAppointmentFrom(Appointment oldAppointment){
+        Appointment appointment = new Appointment();
+        appointment.setService(oldAppointment.getService());
+        appointment.setProviders(oldAppointment.getProviders());
+        appointment.setStatus(oldAppointment.getStatus());
+        appointment.setEndDateTime(oldAppointment.getEndDateTime());
+        appointment.setStartDateTime(oldAppointment.getStartDateTime());
+        appointment.setServiceType(oldAppointment.getServiceType());
+        appointment.setPatient(oldAppointment.getPatient());
+        appointment.setLocation(oldAppointment.getLocation());
+        appointment.setAppointmentKind(oldAppointment.getAppointmentKind());
+        appointment.setComments(oldAppointment.getComments());
+        appointment.setAppointmentAudits(new HashSet<>());
+        return appointment;
+    }
+
 }
--- omod/src/test/java/org/openmrs/module/appointments/web/mapper/SingleAppointmentRecurringPatternMapperTest.java
+++ omod/src/test/java/org/openmrs/module/appointments/web/mapper/SingleAppointmentRecurringPatternMapperTest.java
@@ -7,20 +7,18 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.openmrs.api.APIException;
-import org.openmrs.module.appointments.helper.AppointmentServiceHelper;
 import org.openmrs.module.appointments.model.Appointment;
-import org.openmrs.module.appointments.model.AppointmentAudit;
 import org.openmrs.module.appointments.model.AppointmentRecurringPattern;
 import org.openmrs.module.appointments.service.AppointmentsService;
 import org.openmrs.module.appointments.web.contract.AppointmentRequest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.*;
 
 @RunWith(PowerMockRunner.class)
@@ -32,9 +30,6 @@ public class SingleAppointmentRecurringPatternMapperTest {
     @Mock
     private AppointmentMapper appointmentMapper;
 
-    @Mock
-    private AppointmentServiceHelper appointmentServiceHelper;
-
     @InjectMocks
     SingleAppointmentRecurringPatternMapper singleAppointmentRecurringPatternMapper;
     @Rule
@@ -42,18 +37,16 @@ public class SingleAppointmentRecurringPatternMapperTest {
 
 
     @Test
-    public void shouldReturnAppointmentRecurringPatternWithAnAppointmentReferringToTheAppointmentInAppointmentRequest() throws IOException {
+    public void shouldReturnAppointmentRecurringPatternWithAnAppointmentReferringToTheAppointmentInAppointmentRequest() {
         AppointmentRequest appointmentRequest = mock(AppointmentRequest.class);
         Appointment appointment = mock(Appointment.class);
         AppointmentRecurringPattern appointmentRecurringPattern = mock(AppointmentRecurringPattern.class);
-        String notes = "";
-        AppointmentAudit appointmentAuditMock = mock(AppointmentAudit.class);
-
 
         when(appointmentRequest.getUuid()).thenReturn("uuid");
 
         when(appointmentsService.getAppointmentByUuid("uuid")).thenReturn(appointment);
         doNothing().when(appointmentMapper).mapAppointmentRequestToAppointment(eq(appointmentRequest), any(Appointment.class));
+
 
         when(appointment.getAppointmentRecurringPattern()).thenReturn(appointmentRecurringPattern);
         when(appointmentRecurringPattern.getAppointments()).thenReturn(new HashSet<>(Arrays.asList(appointment)));
@@ -62,6 +55,7 @@ public class SingleAppointmentRecurringPatternMapperTest {
 
         verify(appointmentRequest, times(1)).getUuid();
         verify(appointmentsService, times(1)).getAppointmentByUuid("uuid");
+
         verify(appointment, times(1)).getAppointmentRecurringPattern();
         verify(appointmentRecurringPattern, times(1)).getAppointments();
         verify(appointmentMapper, times(1)).mapAppointmentRequestToAppointment(eq(appointmentRequest), any());
@@ -76,6 +70,7 @@ public class SingleAppointmentRecurringPatternMapperTest {
         assertEquals(2, returnedAppointmentRecurringPattern.getAppointments().size());
         assertEquals(appointment, newAppointment.getRelatedAppointment());
         assertNull(newAppointment.getId());
+        assertEquals(0, newAppointment.getAppointmentAudits().size());
         assertEquals(null, newAppointment.getAppointmentNumber());
         assertEquals(appointment, oldAppointment);
 
@@ -93,97 +88,4 @@ public class SingleAppointmentRecurringPatternMapperTest {
         singleAppointmentRecurringPatternMapper.fromRequest(appointmentRequest);
     }
 
-    @Test
-    public void shouldVoidTheOldAppointmentWhenNewAppointmentIsCreatedFromAppointmentRequest() {
-        AppointmentRequest appointmentRequest = mock(AppointmentRequest.class);
-        Appointment appointment = new Appointment();
-        AppointmentRecurringPattern appointmentRecurringPattern = mock(AppointmentRecurringPattern.class);
-        appointment.setAppointmentRecurringPattern(appointmentRecurringPattern);
-        appointment.setVoided(false);
-
-        when(appointmentRequest.getUuid()).thenReturn("uuid");
-
-        when(appointmentsService.getAppointmentByUuid("uuid")).thenReturn(appointment);
-        doNothing().when(appointmentMapper).mapAppointmentRequestToAppointment(eq(appointmentRequest), any(Appointment.class));
-
-        when(appointmentRecurringPattern.getAppointments()).thenReturn(new HashSet<>(Arrays.asList(appointment)));
-
-        singleAppointmentRecurringPatternMapper.fromRequest(appointmentRequest);
-        assertEquals(true, appointment.getVoided());
-    }
-
-    @Test
-    public void shouldNotCreateANewRelatedAppointmentWhenTheAppointmentAlreadyHasARelatedAppointment(){
-        AppointmentRequest appointmentRequest = mock(AppointmentRequest.class);
-        Appointment appointment = mock(Appointment.class);
-        Appointment relatedAppointment = mock(Appointment.class);
-        AppointmentRecurringPattern appointmentRecurringPattern = mock(AppointmentRecurringPattern.class);
-
-        when(appointmentRequest.getUuid()).thenReturn("uuid");
-
-        when(appointmentsService.getAppointmentByUuid("uuid")).thenReturn(appointment);
-        when(appointment.getRelatedAppointment()).thenReturn(relatedAppointment);
-        when(appointment.getAppointmentRecurringPattern()).thenReturn(appointmentRecurringPattern);
-        when(appointmentRecurringPattern.getAppointments()).thenReturn(new HashSet<>(Arrays.asList(appointment, relatedAppointment)));
-        assertEquals(2, appointmentRecurringPattern.getAppointments().size());
-
-        final AppointmentRecurringPattern returnedAppointmentRecurringPattern = singleAppointmentRecurringPatternMapper.fromRequest(appointmentRequest);
-
-        verify(appointment, times(1)).getRelatedAppointment();
-        verify(appointmentMapper, times(1))
-                .mapAppointmentRequestToAppointment(appointmentRequest,appointment);
-        assertEquals(2, returnedAppointmentRecurringPattern.getAppointments().size());
-
-    }
-
-    @Test
-    public void shouldAddTheAuditLogInTheNewAppointmentCreatedFromSingleAppointmentRequest() throws IOException {
-        AppointmentRequest appointmentRequest = mock(AppointmentRequest.class);
-        Appointment appointment = mock(Appointment.class);
-        AppointmentRecurringPattern appointmentRecurringPattern = mock(AppointmentRecurringPattern.class);
-        String notes = "";
-        AppointmentAudit appointmentAuditMock = mock(AppointmentAudit.class);
-
-
-        when(appointmentRequest.getUuid()).thenReturn("uuid");
-
-        when(appointmentsService.getAppointmentByUuid("uuid")).thenReturn(appointment);
-        doNothing().when(appointmentMapper).mapAppointmentRequestToAppointment(eq(appointmentRequest), any(Appointment.class));
-
-        when(appointmentServiceHelper.getAppointmentAsJsonString(appointment)).thenReturn(notes);
-        when(appointmentServiceHelper.getAppointmentAuditEvent(appointment, notes))
-                .thenReturn(appointmentAuditMock);
-        when(appointment.getAppointmentRecurringPattern()).thenReturn(appointmentRecurringPattern);
-        when(appointmentRecurringPattern.getAppointments()).thenReturn(new HashSet<>(Arrays.asList(appointment)));
-
-        final AppointmentRecurringPattern returnedAppointmentRecurringPattern = singleAppointmentRecurringPatternMapper.fromRequest(appointmentRequest);
-        final ArrayList<Appointment> finalAppointments = new ArrayList<>(returnedAppointmentRecurringPattern.getAppointments());
-        final Appointment appointment1 = finalAppointments.get(0);
-        final Appointment appointment2 = finalAppointments.get(1);
-
-        final Appointment newAppointment = appointment1.getId() == null ? appointment1 : appointment2;
-        final Appointment oldAppointment = appointment1.getId() != null ? appointment1 : appointment2;
-        assertNotNull(newAppointment.getAppointmentAudits());
-
-    }
-
-    @Test
-    public void shouldAddTheAuditLogInTheExisitingAppointmentCreatedFromSingleAppointmentRequest() throws IOException {
-        AppointmentRequest appointmentRequest = mock(AppointmentRequest.class);
-        Appointment appointment = mock(Appointment.class);
-        Appointment relatedAppointment = mock(Appointment.class);
-        AppointmentRecurringPattern appointmentRecurringPattern = mock(AppointmentRecurringPattern.class);
-
-        when(appointmentRequest.getUuid()).thenReturn("uuid");
-
-        when(appointmentsService.getAppointmentByUuid("uuid")).thenReturn(appointment);
-        when(appointment.getRelatedAppointment()).thenReturn(relatedAppointment);
-        when(appointment.getAppointmentRecurringPattern()).thenReturn(appointmentRecurringPattern);
-        when(appointmentRecurringPattern.getAppointments()).thenReturn(new HashSet<>(Arrays.asList(appointment, relatedAppointment)));
-        assertEquals(2, appointmentRecurringPattern.getAppointments().size());
-
-        singleAppointmentRecurringPatternMapper.fromRequest(appointmentRequest);
-        assertNotNull(appointment.getAppointmentAudits());
-
-    }
 }
