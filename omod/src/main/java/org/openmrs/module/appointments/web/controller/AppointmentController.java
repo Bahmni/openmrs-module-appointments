980652e7b14dd6b84d6d7a8147b94ff76af9584b
venkatavineela <vineela.vini421.vv@gmail.com>
2019-07-24 15:37:34 +0530

    Vineela, Sneha Bagri | MOBN-576 | Replace == with equals method in editAppointment from AppointmentController

--- omod/src/main/java/org/openmrs/module/appointments/web/controller/AppointmentController.java
+++ omod/src/main/java/org/openmrs/module/appointments/web/controller/AppointmentController.java
@@ -258,8 +258,8 @@ public class AppointmentController {
                     while(iterator.hasNext()){
                         final Appointment currentAppointment = iterator.next();
                         final Appointment relatedAppointment = currentAppointment.getRelatedAppointment();
-                        if ((currentAppointment.getUuid() == appointmentRequestUuid && !currentAppointment.getVoided())
-                                || (relatedAppointment!=null && relatedAppointment.getUuid() == appointmentRequestUuid)) {
+                        if ((currentAppointment.getUuid().equals(appointmentRequestUuid) && !currentAppointment.getVoided())
+                                || (relatedAppointment!=null && relatedAppointment.getUuid().equals(appointmentRequestUuid))) {
                             updatedAppointment = currentAppointment;
                             break;
                         }
