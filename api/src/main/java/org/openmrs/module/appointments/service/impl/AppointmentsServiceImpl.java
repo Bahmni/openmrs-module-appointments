MSF-OCB/maadi-openmrs-module-appointments

Sowmika, Saikumar | [Reset Appointment Status] | Authenticate user to reset status to scheduled

api/src/main/java/org/openmrs/module/appointments/service/impl/AppointmentsServiceImpl.java
@@ -33,6 +33,8 @@ import static java.util.Objects.isNull;
 public class AppointmentsServiceImpl implements AppointmentsService {
 
     private static final String MANAGE_APPOINTMENTS_PRIVILEGE = "Manage Appointments";
+    private static final String RESET_APPOINTMENT_STATUS = "Reset Appointment Status";
+    private static final String PRIVILEGES_EXCEPTION_CODE = "error.privilegesRequired";
 
     AppointmentDao appointmentDao;
 
@@ -152,21 +154,24 @@ public class AppointmentsServiceImpl implements AppointmentsService {
     }
 
     @Override
-    public void changeStatus(Appointment appointment, String status, Date onDate) throws APIException{
+    public void changeStatus(Appointment appointment, String status, Date onDate) throws APIException {
         if (!validateUserAccess(appointment)) {
-            throw new APIAuthenticationException(Context.getMessageSourceService().getMessage("error.privilegesRequired",
-                    new Object[] { MANAGE_APPOINTMENTS_PRIVILEGE }, null));
+            throw new APIAuthenticationException(Context.getMessageSourceService().getMessage(PRIVILEGES_EXCEPTION_CODE,
+                    new Object[]{MANAGE_APPOINTMENTS_PRIVILEGE}, null));
+        }
+        AppointmentStatus appointmentStatus = AppointmentStatus.valueOf(status);
+        if (appointmentStatus == AppointmentStatus.Scheduled && !Context.hasPrivilege(RESET_APPOINTMENT_STATUS)) {
+            throw new APIAuthenticationException(Context.getMessageSourceService().getMessage(PRIVILEGES_EXCEPTION_CODE,
+                    new Object[]{RESET_APPOINTMENT_STATUS}, null));
         }
         List<String> errors = new ArrayList<>();
-        AppointmentStatus appointmentStatus = AppointmentStatus.valueOf(status);
         validateStatusChange(appointment, appointmentStatus, errors);
-        if(errors.isEmpty()) {
+        if (errors.isEmpty()) {
             appointment.setStatus(appointmentStatus);
             appointmentDao.save(appointment);
-            String notes = onDate != null ? onDate.toInstant().toString(): null;
+            String notes = onDate != null ? onDate.toInstant().toString() : null;
             createEventInAppointmentAudit(appointment, notes);
-        }
-        else {
+        } else {
             String message = StringUtils.join(errors, "\n");
             throw new APIException(message);
         }

Bahmni/openmrs-module-appointments

api/src/main/java/org/openmrs/module/appointments/service/impl/AppointmentsServiceImpl.java
@@ -40,6 +40,8 @@ public class AppointmentsServiceImpl implements AppointmentsService {
 
     private static final int EMPTY_SET_SIZE = 0;
     private Log log = LogFactory.getLog(this.getClass());
+    private static final String RESET_APPOINTMENT_STATUS = "Reset Appointment Status";
+    private static final String PRIVILEGES_EXCEPTION_CODE = "error.privilegesRequired";
 
     AppointmentDao appointmentDao;
 
@@ -172,21 +174,24 @@ public class AppointmentsServiceImpl implements AppointmentsService {
     }
 
     @Override
-    public void changeStatus(Appointment appointment, String status, Date onDate) throws APIException{
+    public void changeStatus(Appointment appointment, String status, Date onDate) throws APIException {
         if (!validateIfUserHasSelfOrAllAppointmentsAccess(appointment)) {
-            throw new APIAuthenticationException(Context.getMessageSourceService().getMessage("error.privilegesRequired",
-                    new Object[] { MANAGE_APPOINTMENTS }, null));
+            throw new APIAuthenticationException(Context.getMessageSourceService().getMessage(PRIVILEGES_EXCEPTION_CODE,
+                    new Object[]{MANAGE_APPOINTMENTS}, null));
+        }
+        AppointmentStatus appointmentStatus = AppointmentStatus.valueOf(status);
+        if (appointmentStatus == AppointmentStatus.Scheduled && !Context.hasPrivilege(RESET_APPOINTMENT_STATUS)) {
+            throw new APIAuthenticationException(Context.getMessageSourceService().getMessage(PRIVILEGES_EXCEPTION_CODE,
+                    new Object[]{RESET_APPOINTMENT_STATUS}, null));
         }
         List<String> errors = new ArrayList<>();
-        AppointmentStatus appointmentStatus = AppointmentStatus.valueOf(status);
         validateStatusChange(appointment, appointmentStatus, errors);
-        if(errors.isEmpty()) {
+        if (errors.isEmpty()) {
             appointment.setStatus(appointmentStatus);
             appointmentDao.save(appointment);
-            String notes = onDate != null ? onDate.toInstant().toString(): null;
+            String notes = onDate != null ? onDate.toInstant().toString() : null;
             createEventInAppointmentAudit(appointment, notes);
-        }
-        else {
+        } else {
             String message = StringUtils.join(errors, "\n");
             throw new APIException(message);
         }