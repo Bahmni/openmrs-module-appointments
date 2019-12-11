aa72df1f95810d5eec2c0dadafb2a3dff20196c7
sukreet <sukreet001@gmail.com>
2019-12-11 14:54:56 +0530

    Sukreet | AP-29 | Removed hardcodings for privilege name.

--- api/src/main/java/org/openmrs/module/appointments/service/AppointmentServiceDefinitionService.java
+++ api/src/main/java/org/openmrs/module/appointments/service/AppointmentServiceDefinitionService.java
@@ -9,30 +9,33 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Date;
 import java.util.List;
 
+import static org.openmrs.module.appointments.constants.PrivilegeConstants.MANAGE_APPOINTMENTS_SERVICE;
+import static org.openmrs.module.appointments.constants.PrivilegeConstants.VIEW_APPOINTMENTS_SERVICE;
+
 public interface AppointmentServiceDefinitionService {
 
     @Transactional
-    @Authorized({"Manage Appointment Services"})
+    @Authorized({MANAGE_APPOINTMENTS_SERVICE})
     AppointmentServiceDefinition save(AppointmentServiceDefinition appointmentServiceDefinition);
 
     @Transactional
-    @Authorized({"View Appointment Services", "Manage Appointment Services"})
+    @Authorized({VIEW_APPOINTMENTS_SERVICE, MANAGE_APPOINTMENTS_SERVICE})
     List<AppointmentServiceDefinition> getAllAppointmentServices(boolean includeVoided);
 
     @Transactional
-    @Authorized({"View Appointment Services", "Manage Appointment Services"})
+    @Authorized({VIEW_APPOINTMENTS_SERVICE, MANAGE_APPOINTMENTS_SERVICE})
     AppointmentServiceDefinition getAppointmentServiceByUuid(String uuid);
 
     @Transactional
-    @Authorized({"Manage Appointment Services"})
+    @Authorized({MANAGE_APPOINTMENTS_SERVICE})
     AppointmentServiceDefinition voidAppointmentService(AppointmentServiceDefinition appointmentServiceDefinition, String voidReason);
 
     @Transactional
-    @Authorized({"View Appointment Services", "Manage Appointment Services"})
+    @Authorized({VIEW_APPOINTMENTS_SERVICE, MANAGE_APPOINTMENTS_SERVICE})
     AppointmentServiceType getAppointmentServiceTypeByUuid(String serviceTypeUuid);
 
     @Transactional
-    @Authorized({"View Appointment Services", "Manage Appointment Services"})
-	Integer calculateCurrentLoad(AppointmentServiceDefinition appointmentServiceDefinition, Date startDateTime, Date endDateTime);
+    @Authorized({VIEW_APPOINTMENTS_SERVICE, MANAGE_APPOINTMENTS_SERVICE})
+    Integer calculateCurrentLoad(AppointmentServiceDefinition appointmentServiceDefinition, Date startDateTime, Date endDateTime);
 }
 
