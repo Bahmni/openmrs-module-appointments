a58a4fe273d40c6398fe8f1e4c1b9a2ce3a069d2
sowmika <sowmika148@gmail.com>
2019-12-30 16:42:08 +0530

    Sowmika, Bindu | MOBN-927 | Create event records for recurring appointments creation

--- api/src/main/java/org/openmrs/module/appointments/advice/RecurringAppointmentsAdvice.java
+++ api/src/main/java/org/openmrs/module/appointments/advice/RecurringAppointmentsAdvice.java
@@ -2,38 +2,38 @@ package org.openmrs.module.appointments.advice;
 
 import org.openmrs.api.context.Context;
 import org.openmrs.module.appointments.model.Appointment;
+import org.openmrs.module.appointments.model.AppointmentRecurringPattern;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 
-import static java.util.Objects.isNull;
 import static org.openmrs.module.appointments.constants.AppointmentsEventRecordsConstants.CATEGORY;
 import static org.openmrs.module.appointments.constants.AppointmentsEventRecordsConstants.DEFAULT_URL_PATTERN;
 import static org.openmrs.module.appointments.constants.AppointmentsEventRecordsConstants.RAISE_EVENT_GLOBAL_PROPERTY;
 import static org.openmrs.module.appointments.constants.AppointmentsEventRecordsConstants.URL_PATTERN_GLOBAL_PROPERTY;
 
-public class AppointmentAdvice extends AbstractBaseAdvice {
+public class RecurringAppointmentsAdvice extends AbstractBaseAdvice {
 
-    public static final String TITLE = "Appointment";
-    private static final ArrayList<String> METHOD_NAMES = new ArrayList<>(Arrays.asList("validateAndSave", "changeStatus", "undoStatusChange"));
-    private static final ArrayList<String> VOIDED_METHOD_NAMES = new ArrayList<>(Arrays.asList("changeStatus", "undoStatusChange"));
+    public static final String TITLE = "RecurringAppointment";
+    public static final String VALIDATE_AND_SAVE = "validateAndSave";
+    private static final ArrayList<String> METHOD_NAMES = new ArrayList<>(Collections.singletonList(VALIDATE_AND_SAVE));
+
+    @Override
+    public void afterReturning(Object returnValue, Method method, Object[] arguments, Object target) throws Throwable {
+        Object processedReturnValue = returnValue;
+        if (VALIDATE_AND_SAVE.equals(method.getName())) {
+            AppointmentRecurringPattern appointmentRecurringPattern = (AppointmentRecurringPattern) processedReturnValue;
+            for (Appointment appointment : appointmentRecurringPattern.getAppointments()) {
+                super.afterReturning(appointment, method, arguments, target);
+            }
+        }
+    }
 
     @Override
     protected String getContents(Object returnValue) {
         return getUrlPattern(URL_PATTERN_GLOBAL_PROPERTY, DEFAULT_URL_PATTERN)
                 .replace("{uuid}", ((Appointment) returnValue).getUuid());
-    }
-
-    @Override
-    public void afterReturning(Object returnValue, Method method, Object[] arguments, Object target) throws Throwable {
-        // Hacky fix for void method
-        Object processedReturnValue = returnValue;
-
-        if (VOIDED_METHOD_NAMES.contains(method.getName()) && !isNull(arguments))
-            processedReturnValue = arguments[0];
-
-        super.afterReturning(processedReturnValue, method, arguments, target);
     }
 
     @Override
---
+++ api/src/main/java/org/openmrs/module/appointments/constants/AppointmentsEventRecordsConstants.java
@@ -0,0 +1,8 @@
+package org.openmrs.module.appointments.constants;
+
+public class AppointmentsEventRecordsConstants {
+    public static final String CATEGORY = "appointments";
+    public static final String DEFAULT_URL_PATTERN = "/openmrs/ws/rest/v1/appointment?uuid={uuid}";
+    public static final String RAISE_EVENT_GLOBAL_PROPERTY = "atomfeed.publish.eventsForAppointments";
+    public static final String URL_PATTERN_GLOBAL_PROPERTY = "atomfeed.event.urlPatternForAppointments";
+}
