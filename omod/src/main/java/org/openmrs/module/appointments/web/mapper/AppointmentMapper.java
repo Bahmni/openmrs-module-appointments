5cbfaa894d3e9cd7cc739b43fcf8373a54f51d5f
Gracil Benedict <gracilb@thoughtworks.com>
2020-08-31 11:16:01 +0530

    CON-19 |  Gracil/Hamza | Add isTeleconsultationEnabled field to the appointment module

--- omod/src/main/java/org/openmrs/module/appointments/web/mapper/AppointmentMapper.java
+++ omod/src/main/java/org/openmrs/module/appointments/web/mapper/AppointmentMapper.java
@@ -100,6 +100,7 @@ public class AppointmentMapper {
         appointment.setStartDateTime(appointmentRequest.getStartDateTime());
         appointment.setEndDateTime(appointmentRequest.getEndDateTime());
         appointment.setAppointmentKind(AppointmentKind.valueOf(appointmentRequest.getAppointmentKind()));
+        appointment.setIsTeleconsultationEnabled(appointmentRequest.getIsTeleconsultationEnabled());
         appointment.setComments(appointmentRequest.getComments());
         mapProvidersForAppointment(appointment, appointmentRequest.getProviders());
     }
@@ -199,6 +200,7 @@ public class AppointmentMapper {
         response.setAppointmentKind(a.getAppointmentKind().name());
         response.setStatus(a.getStatus().name());
         response.setComments(a.getComments());
+        response.setIsTeleconsultationEnabled(a.getIsTeleconsultationEnabled());
         if (appointmentResponseExtension != null)
             response.setAdditionalInfo(appointmentResponseExtension.run(a));
         response.setProviders(mapAppointmentProviders(a.getProviders()));
