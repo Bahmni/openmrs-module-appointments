5e934d94296b350f480631b965b1e42235ff3442
icrc-toliveira <toliveira@icrc.org>
2021-03-30 13:52:00 +0100

    Reverted change on files patientPastAppointments.sql, patientPastAppointments_v2.sql, patientUpcomingAppointments.sql, patientUpcomingAppointments_v2.sql Added files patientPastAppointments_v3.sql, patientPastAppointments_v4.sql, patientUpcomingAppointments_v3.sql, patientUpcomingAppointments_v4.sql
    Changed the liquidbase file from using runOnChange="true" to failOnError="false"
    
    This way, if the first files fail to escape characters the new version can run

--- api/src/main/resources/patientUpcomingAppointments_v2.sql
+++ api/src/main/resources/patientUpcomingAppointments_v2.sql
@@ -1,13 +1,14 @@
 UPDATE global_property
-SET property_value= 'SELECT
+SET property_value= "SELECT
   app_service.uuid,
   app_service.name                                                                                AS `DASHBOARD_APPOINTMENTS_SERVICE_KEY`,
   app_service_type.name                                                                           AS `DASHBOARD_APPOINTMENTS_SERVICE_TYPE_KEY`,
-  DATE_FORMAT(start_date_time, "%d/%m/%Y")                                                        AS `DASHBOARD_APPOINTMENTS_DATE_KEY`,
-  CONCAT(DATE_FORMAT(start_date_time, "%l:%i %p"), " - ", DATE_FORMAT(end_date_time, "%l:%i %p")) AS `DASHBOARD_APPOINTMENTS_SLOT_KEY`,
+  DATE_FORMAT(start_date_time, \"%d/%m/%Y\")                                                        AS `DASHBOARD_APPOINTMENTS_DATE_KEY`,
+  CONCAT(DATE_FORMAT(start_date_time, \"%l:%i %p\"), \" - \", DATE_FORMAT(end_date_time, \"%l:%i %p\")) AS `DASHBOARD_APPOINTMENTS_SLOT_KEY`,
   CONCAT(pn.given_name, ' ', pn.family_name)                                                      AS `DASHBOARD_APPOINTMENTS_PROVIDER_KEY`,
   pa.status                                                                                       AS `DASHBOARD_APPOINTMENTS_STATUS_KEY`,
   pa.teleconsultation                                                                             AS `DASHBOARD_APPOINTMENTS_TELECONSULTATION`
+
 FROM
   patient_appointment pa
   JOIN person p ON p.person_id = pa.patient_id AND pa.voided IS FALSE
@@ -21,5 +22,5 @@ FROM
 WHERE p.uuid = ${patientUuid} AND
       start_date_time >= CURDATE() AND
       (app_service_type.voided IS FALSE OR app_service_type.voided IS NULL)
-ORDER BY start_date_time ASC;'
-WHERE property='bahmni.sqlGet.upComingAppointments';
+ORDER BY start_date_time ASC;"
+WHERE property='bahmni.sqlGet.upComingAppointments'
