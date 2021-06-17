f620ce50ab0beec4e7093851fd307fa9809b6344
icrc-toliveira <toliveira@icrc.org>
2021-06-17 11:18:38 +0100

    AP-77 - Revert removal of left join inside patientPastAppointments.sql / patientUpcomingAppointments.sql query

--- api/src/main/resources/patientUpcomingAppointments.sql
+++ api/src/main/resources/patientUpcomingAppointments.sql
@@ -12,6 +12,7 @@ FROM
   JOIN person p ON p.person_id = pa.patient_id AND pa.voided IS FALSE
   JOIN appointment_service app_service
     ON app_service.appointment_service_id = pa.appointment_service_id AND app_service.voided IS FALSE
+  LEFT JOIN patient_appointment_provider pap on pa.patient_appointment_id = pap.patient_appointment_id AND (pap.voided=0 OR pap.voided IS NULL)
   LEFT JOIN provider prov ON prov.provider_id = pap.provider_id AND prov.retired IS FALSE
   LEFT JOIN person_name pn ON pn.person_id = prov.person_id AND pn.voided IS FALSE
   LEFT JOIN appointment_service_type app_service_type
