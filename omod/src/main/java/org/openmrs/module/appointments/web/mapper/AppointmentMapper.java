3723c1389394c6701afffa52a40bdbe7e4c1948c
CynthiaKamau <CynthiaKamau54@gmail.com>
2022-12-08 21:35:55 +0300

    Added unique patient number as default identifier

--- omod/src/main/java/org/openmrs/module/appointments/web/mapper/AppointmentMapper.java
+++ omod/src/main/java/org/openmrs/module/appointments/web/mapper/AppointmentMapper.java
@@ -27,6 +27,7 @@ import org.openmrs.module.webservices.rest.web.response.ConversionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.openmrs.PersonAttribute;
+import org.openmrs.PatientIdentifier;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -59,7 +60,6 @@ public class AppointmentMapper {
 
     @Autowired(required = false)
     AppointmentResponseExtension appointmentResponseExtension;
-
     private Log log = LogFactory.getLog(this.getClass());
 
     public List<AppointmentDefaultResponse> constructResponse(List<Appointment> appointments) {
@@ -274,6 +274,13 @@ public class AppointmentMapper {
         map.put("uuid", p.getUuid());
         map.put("identifier", p.getPatientIdentifier().getIdentifier());
         map.putAll(p.getActiveIdentifiers().stream().filter(e -> e.getIdentifierType() != null).collect(Collectors.toMap(e -> e.getIdentifierType().toString().replaceAll("[- ]", ""), e -> e.getIdentifier())));
+        for (PatientIdentifier patientIdentifier : p.getIdentifiers()) {
+            if (patientIdentifier.getIdentifierType().getName().equalsIgnoreCase("Unique Patient Number")) {
+                map.put("identifier", patientIdentifier.getIdentifier());
+            } else (patientIdentifier.getIdentifierType().getName().equalsIgnoreCase("Patient Clinic Number")) {
+                map.put("identifier", patientIdentifier.getIdentifier());
+            }
+        }
         map.put("gender", p.getGender());
         PersonAttribute patientPhoneAttribute = p.getPerson().getAttribute("Telephone contact");
         if (patientPhoneAttribute != null) {
