70a99c4fcfd24b85b7a29d30f852d9b053724329
Makombe Kennedy <kennedymakombe@gmail.com>
2023-01-16 13:25:28 +0300

    Add end point for appointment calendar (#5)
    Co-authored-by: Makombe <makombe>

--- omod/src/main/java/org/openmrs/module/appointments/web/contract/AppointmentsSummary.java
+++ omod/src/main/java/org/openmrs/module/appointments/web/contract/AppointmentsSummary.java
@@ -4,16 +4,36 @@ import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 import java.util.Map;
+import java.util.List;
+import java.util.Date;
 
 public class AppointmentsSummary {
     private AppointmentServiceDefaultResponse appointmentService;
     private Map<String, DailyAppointmentServiceSummary> appointmentCountMap;
+
+    private List<DailyAppointmentServiceSummary> services;
+
+
+    private String appointmentDate;
+
+
+
+    private Integer totalServiceCount;
 
     @JsonCreator
     public AppointmentsSummary(@JsonProperty("appointmentService")AppointmentServiceDefaultResponse appointmentService,
                                @JsonProperty("appointmentCountMap") Map appointmentCountMap) {
         this.appointmentService = appointmentService;
         this.appointmentCountMap = appointmentCountMap;
+    }
+    @JsonCreator
+    public AppointmentsSummary(@JsonProperty("services") List services,
+                               @JsonProperty("appointmentDate") String appointmentDate,
+                               @JsonProperty("dailAappointmentCount") Integer totalServiceCount) {
+        this.services = services;
+        this.appointmentDate = appointmentDate;
+        this.totalServiceCount = totalServiceCount;
+
     }
     public AppointmentServiceDefaultResponse getAppointmentService() {
         return appointmentService;
@@ -30,4 +50,29 @@ public class AppointmentsSummary {
     public void setAppointmentCountMap(Map<String, DailyAppointmentServiceSummary> appointmentCountMap) {
         this.appointmentCountMap = appointmentCountMap;
     }
+
+    public List<DailyAppointmentServiceSummary> getServices() {
+        return services;
+    }
+
+    public void setServices(List<DailyAppointmentServiceSummary> dailyAppointmentList) {
+        this.services = dailyAppointmentList;
+    }
+
+    public String getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(String appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public Integer gettotalServiceCount() {
+        return totalServiceCount;
+    }
+
+    public void settotalServiceCount(Integer totalServiceCount) {
+        this.totalServiceCount = totalServiceCount;
+    }
+
 }
