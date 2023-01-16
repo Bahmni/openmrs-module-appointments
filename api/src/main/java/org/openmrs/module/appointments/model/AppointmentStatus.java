e7e304a97133c58a1d56e92b8e374bf0803bb4f3
Patrick Waweru <patrick.waweru@thepalladiumgroup.com>
2023-01-16 13:03:02 +0300

    1. KHP3-2743 - Pending appointments 2. KHP3-2744 - Honoured appointments 3. KHP3-2745 - Came early appointments 4. KHP3-2746 - Rescheduled appointments

--- api/src/main/java/org/openmrs/module/appointments/model/AppointmentStatus.java
+++ api/src/main/java/org/openmrs/module/appointments/model/AppointmentStatus.java
@@ -1,7 +1,7 @@
 package org.openmrs.module.appointments.model;
 
 public enum AppointmentStatus {
-    Requested("Requested", 0), Scheduled("Scheduled", 1), CheckedIn("CheckedIn", 2), Completed("Completed", 3), Cancelled("Cancelled", 3), Missed("Missed", 3);
+    Requested("Requested", 0), Scheduled("Scheduled", 1), CheckedIn("CheckedIn", 2), Completed("Completed", 3), Cancelled("Cancelled", 3), Missed("Missed", 3), Rescheduled("rescheduled", 3);
 
     private final String value;
     private final int sequence;
