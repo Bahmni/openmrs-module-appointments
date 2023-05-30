0224fd1676fb509529e9e18059adce0d7ec8d48b
Donald Kibet <chelashawdonald@yahoo.com>
2023-05-30 15:15:39 +0300

    (enhc) Add patient idenfifier object to appointment object

--- api/src/main/java/org/openmrs/module/appointments/util/PatientUtil.java
+++ api/src/main/java/org/openmrs/module/appointments/util/PatientUtil.java
@@ -1,35 +1,53 @@
 package org.openmrs.module.appointments.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import org.openmrs.Patient;
-import org.openmrs.PatientIdentifier;
 import org.openmrs.PersonAttribute;
 
 public class PatientUtil {
 
-    public static Map patientMap(Patient p) {
-        Map map = new HashMap();
-        map.put("name", p.getPersonName().getFullName());
-        map.put("uuid", p.getUuid());
-        for (PatientIdentifier patientIdentifier : p.getIdentifiers()) {
-            if (patientIdentifier.getIdentifierType().getName().equalsIgnoreCase("Unique Patient Number")) {
-                map.put("identifier", patientIdentifier.getIdentifier());
-            } else if (patientIdentifier.getIdentifierType().getName().equalsIgnoreCase("Patient Clinic Number")) {
-                map.put("identifier", patientIdentifier.getIdentifier());
+    private static final String IDENTIFIER_NAME = "identifierName";
+    private static final String IDENTIFIER = "identifier";
+    private static final String TELEPHONE_CONTACT = "Telephone contact";
+
+    public static Map<String, Object> patientMap(Patient patient) {
+        Map<String, Object> map = new HashMap<>();
+        try {
+            if (Objects.nonNull(patient)) {
+                if (Objects.nonNull(patient.getPersonName())) {
+                    map.put("name", patient.getPersonName().getFullName());
+                }
+                map.put("uuid", patient.getUuid());
+
+                List<Map<String, String>> identifierList = new ArrayList<>();
+                patient.getIdentifiers().stream()
+                        .filter(Objects::nonNull)
+                        .forEach(patientIdentifier -> {
+                            Map<String, String> identifierMap = new HashMap<>();
+                            identifierMap.put(IDENTIFIER_NAME, patientIdentifier.getIdentifierType().getName());
+                            identifierMap.put(IDENTIFIER, patientIdentifier.getIdentifier());
+                            identifierList.add(identifierMap);
+                        });
+                map.put("identifiers", identifierList);
+                map.put("gender", patient.getGender());
+
+                PersonAttribute patientPhoneAttribute = patient.getPerson().getAttribute(TELEPHONE_CONTACT);
+                String phoneNumber = Objects.nonNull(patientPhoneAttribute) ? patientPhoneAttribute.getValue() : "";
+                map.put("phoneNumber", phoneNumber);
+                map.put("dob", patient.getBirthdate());
+                map.put("age", patient.getAge());
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Log the exception here
         }
-        map.put("gender", p.getGender());
-        PersonAttribute patientPhoneAttribute = p.getPerson().getAttribute("Telephone contact");
-        if (patientPhoneAttribute != null) {
-            String phone = patientPhoneAttribute.getValue();
-            map.put("phoneNumber", phone);
-        } else {
-            map.put("phoneNumber", "");
-        }
-        map.put("dob", p.getBirthdate());
-        map.put("age", p.getAge());
+
         return map;
     }
+
 }
