034e33d56f44ba5fe1b3492435e3ed5fe0583d6e
Umair Fayaz <omayrfayaz@gmail.com>
2023-09-19 15:57:43 +0530

    Fix | Handle null value for customPersonAttributeTypes

--- omod/src/main/java/org/openmrs/module/appointments/web/mapper/AppointmentMapper.java
+++ omod/src/main/java/org/openmrs/module/appointments/web/mapper/AppointmentMapper.java
@@ -289,7 +289,7 @@ public class AppointmentMapper {
 
     private static Map<String, String> mapAttributeTypes(Patient p) {
         String customPersonAttributeTypes = Context.getAdministrationService().getGlobalProperty("appointments.customPersonAttributeTypes");
-        String[] attributeTypes = customPersonAttributeTypes.split(",");
+        String[] attributeTypes = customPersonAttributeTypes != null ? customPersonAttributeTypes.split(",") : new String[0];
         Map<String, String> customAttributesMap = new HashMap<>();
         for (String attributeType : attributeTypes) {
             if (p.getAttribute(attributeType) != null) {
