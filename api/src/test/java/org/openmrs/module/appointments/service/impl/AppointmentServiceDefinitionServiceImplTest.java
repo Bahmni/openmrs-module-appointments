1146fbaf9bfe8bc524780db033c244ca44d048dd
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-10-23 09:51:51 +0530

    44630 | Add. Mock AppointmentServiceAttributeService In AppointmentServiceDefinitionServiceImplTest

--- api/src/test/java/org/openmrs/module/appointments/service/impl/AppointmentServiceDefinitionServiceImplTest.java
+++ api/src/test/java/org/openmrs/module/appointments/service/impl/AppointmentServiceDefinitionServiceImplTest.java
@@ -10,6 +10,7 @@ import org.openmrs.User;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.appointments.dao.AppointmentServiceDao;
 import org.openmrs.module.appointments.model.*;
+import org.openmrs.module.appointments.service.AppointmentServiceAttributeService;
 import org.openmrs.module.appointments.service.AppointmentsService;
 import org.openmrs.module.appointments.util.DateUtil;
 import org.powermock.api.mockito.PowerMockito;
@@ -44,6 +45,9 @@ public class AppointmentServiceDefinitionServiceImplTest {
     @Mock
     private AppointmentsService appointmentsService;
 
+    @Mock
+    private AppointmentServiceAttributeService appointmentServiceAttributeService;
+
     @InjectMocks
     AppointmentServiceDefinitionServiceImpl appointmentServiceService;
 
@@ -55,6 +59,8 @@ public class AppointmentServiceDefinitionServiceImplTest {
         mockStatic(Context.class);
         authenticatedUser = new User(8);
         PowerMockito.when(Context.getAuthenticatedUser()).thenReturn(authenticatedUser);
+        when(appointmentServiceAttributeService.getAttributesByService(Mockito.anyInt(), Mockito.anyBoolean()))
+                .thenReturn(Collections.emptyList());
     }
 
     @Test
