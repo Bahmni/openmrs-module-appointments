49e95de854fcd3af4e6b1c7077315f3e0d15576f
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-10-28 12:49:10 +0530

    BAH-4234 | Add. Appointment Service Search Request Model

---
+++ api/src/main/java/org/openmrs/module/appointments/model/AppointmentServiceSearchRequest.java
@@ -0,0 +1,44 @@
+package org.openmrs.module.appointments.model;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
+public class AppointmentServiceSearchRequest {
+    private String locationUuid;
+    private String specialityUuid;
+    private Boolean includeVoided = false;
+    private Integer limit = 100;
+
+    public String getLocationUuid() {
+        return locationUuid;
+    }
+
+    public void setLocationUuid(String locationUuid) {
+        this.locationUuid = locationUuid;
+    }
+
+    public String getSpecialityUuid() {
+        return specialityUuid;
+    }
+
+    public void setSpecialityUuid(String specialityUuid) {
+        this.specialityUuid = specialityUuid;
+    }
+
+    public Boolean getIncludeVoided() {
+        return includeVoided;
+    }
+
+    public void setIncludeVoided(Boolean includeVoided) {
+        this.includeVoided = includeVoided;
+    }
+
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+}
