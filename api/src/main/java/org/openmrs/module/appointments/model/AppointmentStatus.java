aaba268ba13e59c1be3cc84d9ba3ab8ea972e1d9
kousalyapt-tw <kousalya.pt@thoughtworks.com>
2025-11-26 15:04:14 +0530

    BN-210 | Update. Appointment status sequence (#185)

--- api/src/main/java/org/openmrs/module/appointments/model/AppointmentStatus.java
+++ api/src/main/java/org/openmrs/module/appointments/model/AppointmentStatus.java
@@ -1,7 +1,7 @@
 package org.openmrs.module.appointments.model;
 
 public enum AppointmentStatus {
-    Requested("Requested", 0), WaitList("WaitList", 0), Scheduled("Scheduled", 1),Arrived("Arrived",2), CheckedIn("CheckedIn", 2), Completed("Completed", 3), Cancelled("Cancelled", 3), Missed("Missed", 3);
+    Requested("Requested", 0), WaitList("WaitList", 0), Scheduled("Scheduled", 1),Arrived("Arrived",2), CheckedIn("CheckedIn", 3), Completed("Completed", 4), Cancelled("Cancelled", 4), Missed("Missed", 4);
 
     private final String value;
     private final int sequence;
