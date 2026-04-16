6ceb05eca162863df68580c16cec7bfc18d694f5
Lingeswaran Subramaniyam <lingeswaran.s@thoughtworks.com>
2026-04-16 17:17:02 +0530

    BAH-4444 BAH-4444 Move from atomfeed event publish to spring EMREvent

--- api/src/main/java/org/openmrs/module/appointments/constants/AppointmentsEventRecordsConstants.java
+++ api/src/main/java/org/openmrs/module/appointments/constants/AppointmentsEventRecordsConstants.java
@@ -2,6 +2,6 @@ package org.openmrs.module.appointments.constants;
 
 public class AppointmentsEventRecordsConstants {
     public static final String CATEGORY = "appointments";
-    public static final String RAISE_EVENT_GLOBAL_PROPERTY = "atomfeed.publish.eventsForAppointments";
+    public static final String RAISE_EVENT_GLOBAL_PROPERTY = "eventoutbox.publish.eventsForAppointments";
 
 }
--- api/src/main/java/org/openmrs/module/appointments/events/publisher/AppointmentEventPublisher.java
+++ api/src/main/java/org/openmrs/module/appointments/events/publisher/AppointmentEventPublisher.java
@@ -3,17 +3,22 @@ package org.openmrs.module.appointments.events.publisher;
 import org.openmrs.module.appointments.events.AppointmentEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppointmentEventPublisher implements ApplicationEventPublisherAware {
     private ApplicationEventPublisher eventPublisher;
-    public void setApplicationEventPublisher(@NonNull ApplicationEventPublisher applicationEventPublisher) {
-        this.eventPublisher = applicationEventPublisher;
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
     }
 
     public void publishEvent(AppointmentEvent event) {
+        this.eventPublisher.publishEvent(event);
+    }
+
+    public void publish(Object event) {
         this.eventPublisher.publishEvent(event);
     }
 }
